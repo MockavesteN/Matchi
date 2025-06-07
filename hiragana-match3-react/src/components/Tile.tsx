42d2120ab8d49c07bf3fce8b6a6b7bfdac2d68ae
MockavesteN <pierre.mellstrom@gmail.com>
2025-06-07 14:06:39 +0200

    Add falling animation and meaning overlay

--- hiragana-match3-react/src/components/Tile.tsx
+++ hiragana-match3-react/src/components/Tile.tsx
@@ -1,5 +1,5 @@
-
 import { Tile as TileType } from "../types";
+import { motion } from "framer-motion";
 
 interface Props {
   tile: TileType;
@@ -9,12 +9,14 @@ interface Props {
 
 export default function Tile({ tile, onClick, selected }: Props) {
   return (
-    <div
+    <motion.div
       onClick={onClick}
-      className={\`tile w-12 h-12 m-0.5 bg-white hover:bg-yellow-100 transition
-        \${selected ? "ring-4 ring-blue-400" : ""}\`}
+      initial={tile.isNew ? { y: -40, opacity: 0 } : false}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.3 }}
+      className={`tile w-12 h-12 m-0.5 bg-white hover:bg-yellow-100 transition ${selected ? "ring-4 ring-blue-400" : ""}`}
     >
       {tile.kana}
-    </div>
+    </motion.div>
   );
 }
